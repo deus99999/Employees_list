48cdc811492dc217fd541ee41c10bbfc537b7e9e
deus99999 <rudenkooleksei@gmail.com>
2023-06-29 22:52:05 +0300

    second commit

--- project/web/views.py
+++ project/web/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 # from .models import Employee
+from .models import Worker
 
 # Create your views here.
 def index(request):
-    return render(request, 'web/base.html')
+    #return render(request, 'web/base.html')
+    nodes = Worker.objects.all()
+    return render(request, "web/base.html", {'nodes': nodes}) #,   context_instance=RequestContext(request))
